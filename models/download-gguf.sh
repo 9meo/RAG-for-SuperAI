e3dfd3761d90f439bca9102a98eaf9025c4636c8
9MeO <sansarn@gmail.com>
2024-05-01 13:53:30 +0700

    add download

--- models/download-gguf.sh
+++ models/download-gguf.sh
@@ -1,1 +1,3 @@
 wget https://huggingface.co/openthaigpt/openthaigpt-1.0.0-7b-chat/resolve/main/openthaigpt-Q4_K_M.gguf
+
+#scp <lanta-user>@transfer.lanta.or.th:/project/lt900048-ai24tn/models/openthaigpt/openthaigpt-1.0.0-7b-chat/openthaigpt-Q4_K_M.gguf .
