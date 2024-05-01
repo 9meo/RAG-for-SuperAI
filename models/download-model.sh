7d830d10d70adab352dc15ee48ebf6871b934dee
9MeO <sansarn@gmail.com>
2024-05-01 14:11:12 +0700

    add download from lanta

--- models/download-model.sh
+++ models/download-model.sh
@@ -1,3 +1,5 @@
 pip install hf_transfer
 export HF_HUB_ENABLE_HF_TRANSFER=1
 huggingface-cli download BAAI/bge-m3 --local-dir bge-m3
+
+#scp -r <lanta-user>@transfer.lanta.nstda.or.th:/project/lt900048-ai24tn/models/BAAI/bge-m3 .
